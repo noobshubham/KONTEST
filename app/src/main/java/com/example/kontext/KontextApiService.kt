837dff04e6b94cb971cafa7a485b8d2708bc6ecf
noobshubham <noobshubham65@gmail.com>
2022-08-28 10:33:49 +0530

    Minor Fix

--- app/src/main/java/com/example/kontext/KontextApiService.kt
+++
@@ -1,3 +0,0 @@
-package com.example.kontext
-
-private const val BASE_URL = "https://kontests.net/api/v1/all"
---
+++ app/src/main/java/com/example/kontext/network/KontextData.kt
@@ -0,0 +1,12 @@
+package com.example.kontext.network
+
+data class KontextData(
+    val name: String,
+    val url: String,
+    val start_time: String,
+    val end_time: String,
+    val duration: String,
+    val site: String,
+    val in_24_hours: String,
+    val status: String
+)
